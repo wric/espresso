c7a7d456cca4ab23f76a006c27a8acdf9c73ebc0
Richard Westberg <westberg.richard@gmail.com>
2021-11-21 22:30:03 +0100

    restructure backend folder

--- espresso.config.js
+++ espresso.config.js
@@ -1,34 +1,32 @@
-module.exports = {
-  apps: [
-    {
-      name: 'marax',
-      namespace: 'espresso',
-      script: 'src/marax.js',
-      env: {
-        DEBUG: true,
-        PORT: 8086,
-        PIN: 17,
-        SERIAL: '/dev/serial0'
-      },
-      env_production: {
-        DEBUG: false,
-        PORT: 8086,
-        PIN: 17,
-        SERIAL: '/dev/serial0'
-      }
+const apps = [
+  {
+    name: 'marax',
+    script: './backend/src/marax.js',
+    env: {
+      DEBUG: true,
+      PORT: 8086,
+      PIN: 17,
+      SERIAL: '/dev/serial0'
     },
-    {
-      name: 'wilfa',
-      namespace: 'espresso',
-      script: 'src/wilfa.js',
-      env: {
-        DEBUG: true,
-        PORT: 8085
-      },
-      env_production: {
-        DEBUG: false,
-        PORT: 8085
-      }
+    env_production: {
+      DEBUG: false,
+      PORT: 8086,
+      PIN: 17,
+      SERIAL: '/dev/serial0'
     }
-  ]
-}
+  },
+  {
+    name: 'wilfa',
+    script: './backend/src/wilfa.js',
+    env: {
+      DEBUG: true,
+      PORT: 8085
+    },
+    env_production: {
+      DEBUG: false,
+      PORT: 8085
+    }
+  }
+]
+
+module.exports = { apps }
